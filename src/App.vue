4b5da786bacf3f964716ad3293eb6deef473791c
Doit <lpylpyleo@gmail.com>
2019-02-24 21:56:56 +0800

    5th

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Card v-for="idol in idols" :title="idol.name" img="https://storage.matsurihi.me/mltd/card/033sih0114_0_b.png"
+    text="nyan nyan" footer="add in fes" :key="idol.id"/>
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue'
+import idols from '../resources/idols'
+
+export default {
+  name: 'app',
+  components: {
+    Card: Card
+  },
+  data() {
+    return {
+      idols:idols
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="card">
+        <img :src="img" class="card-img-top" alt="">
+        <div class="card-body">
+            <div class="card-title">{{title}}</div>
+            <div class="card-text">{{text}}</div>
+        </div>
+        <div class="card-footer">{{footer}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        props: ['img','title','text','footer']
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card{
+    margin-left: 50px;
+    margin-right: 50px;
+}
+</style>
